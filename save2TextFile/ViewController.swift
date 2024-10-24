556c21e9e1b3f7063411d1d70b282b24e950efb6
DomiTheKava <Dominikpenkava@yahoo.com>
2024-10-24 14:18:58 -0500

    V1

--- save2TextFile/ViewController.swift
+++ save2TextFile/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tvTop: UITextView!
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var tvBottom: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tvTop.text = "Type your new text right here"
+        tvBottom.text = ""
     }
 
-
+    
+    @IBAction func btnWriteText(_ sender: Any) {
+        let fm = FileManager.default
+        let urls = fm.urls(for: .documentDirectory, in: .userDomainMask)
+        let url = urls.last?.appendingPathComponent("file.text")
+        do {
+            try tvTop.text.write(to: url!, atomically: true, encoding: .utf8)
+            
+        } catch {
+            print("Error Writing File")
+        }
+    }
+    
+    @IBAction func btnReadText(_ sender: Any) {
+//        tvBottom.text =
+        let fm = FileManager.default
+        let urls = fm.urls(for: .documentDirectory, in: .userDomainMask)
+        let url = urls.last?.appendingPathComponent("file.text")
+        do {
+            let contents = try String(contentsOf: url!, encoding: .utf8)
+            tvBottom.text = contents
+        } catch {
+            print("Error Reading File")
+        }
+    }
+    
+    
 }
 
