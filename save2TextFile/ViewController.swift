dcddbaf0f46d194e7a8bf25d4769c64ecb6e93ff
DomiTheKava <Dominikpenkava@yahoo.com>
2024-10-24 13:48:17 -0500

    Initial Commit

---
+++ save2TextFile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  save2TextFile
+//
+//  Created by Dominik Penkava on 10/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
